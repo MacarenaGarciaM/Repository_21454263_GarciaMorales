3fa72a0e52bcfdaca04db2b4688b5fb266dd80c4
MacarenaGarciaM <macarena.garcia@usach.cl>
2024-04-14 22:29:26 -0400

    Requerimiento pcar

--- Lab1_214542633_GarciaMorales.rkt
+++ Lab1_214542633_GarciaMorales.rkt
@@ -22,8 +22,9 @@
 (define e8 (station 8 "Parque OHiggins" r 30))
 (define e9 (station 9 "San Pablo" t 40))
 (define e10 (station 10 "Los Dominicos" t 60))
-e0
-e1
+
+
+
 ;;REQUERIMIENTO 2: Función que permite establecer enlaces entre dos estaciones.
 ; Dom: point1 (station)  X point2 (station) X distance (positive-number) X cost (positive-number U {0})
 ; Rec: section
@@ -40,8 +41,8 @@ e1
 (define s7 (section e0 e7 3  0))
 (define s8 (section e0 e9 7  100))
 (define s9 (section e6 e10 15  250))
-s0
-s1
+
+
 ;;REQUERIMIENTO 3: Función que permite crear una línea
 ;Dom: id (int) X name (string) X rail-type (string) X section* (* señala que se pueden agregar 0 o más tramos)
 ;Rec: line
@@ -51,8 +52,8 @@ s1
 
 (define l0 (line 0 "Línea 0" "UIC 60 ASCE"))
 (define l1 (line 1 "Línea 1" "100 R.E." s0 s1 s2 s3 s5 s7 s8 s9))
-l0
-l1
+
+
 ;;REQUERIMIENTO 4: Función que permite determinar el largo total de una línea
 ;Dom: line (line)
 ;Rec: positive-number
@@ -63,7 +64,7 @@ l1
 (define line-length
   (lambda (line)
      (apply + (map get-distance(cdr(cdr (cdr line)))))))
-(line-length l1)
+
 
 ;;REQUERIMIENTO 6:  Función que permite determinar el costo total (monetario) de recorrer una línea.
 ;Dom: line (line)
@@ -93,3 +94,21 @@ l1
 (define l3 (line-add-section l2 s1))
 (define l4 (line-add-section l3 s2))
 (define l5 (line-add-section l4 s3))
+
+;;REQUERIMIENTO 10: Permite crear los carros de pasajeros que conforman un convoy. Los carros pueden ser de tipo terminal (tr) o central (ct).
+;Dom: id (int) X capacity (positive integer) X model (string) X type (car-type))
+;Rec: pcar
+(define tr "tr")
+(define ct "ct")
+
+(define pcar
+  (lambda (id capacity model type)
+    (list id capacity model type)))
+(define pc0 (pcar 0 100 "NS-74" ct))
+(define pc1 (pcar 1 100 "NS-74" tr))
+(define pc2 (pcar 2 150  "NS-74" tr))
+(define pc3 (pcar 3 100 "NS-74" ct))
+(define pc4 (pcar 4 100 "AS-2014" ct))
+(define pc5 (pcar 5 100 "AS-2014" ct))
+(define pc6 (pcar 6 100 "AS-2016" ct))
+
