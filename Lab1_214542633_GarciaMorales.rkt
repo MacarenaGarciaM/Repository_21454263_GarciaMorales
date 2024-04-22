2e3c8d2ea174e6c05ac04c1a2ae61cc0d98a43f1
MacarenaGarciaM <macarena.garcia@usach.cl>
2024-04-21 23:52:30 -0400

    requerimientos faltantes y correcciones

--- Lab1_214542633_GarciaMorales.rkt
+++ Lab1_214542633_GarciaMorales.rkt
@@ -59,27 +59,96 @@
 ;Rec: positive-number
 (define get-distance
   (lambda (section)
-    (car(car(cdr section))))) ;;creamos una función para obtener la distancia de las estaciones
+    (car(cdr(cdr section))))) ;;creamos una función para obtener la distancia de las estaciones
 
 (define line-length
   (lambda (line)
      (apply + (map get-distance(cdr(cdr (cdr line)))))))
 
+;;REQUERIMIENTO 6: Función que permite determinar el trayecto entre una estación origen y una final.
+;Dom: line (line) X station1-name (String) X station2-name (String)
+;Rec: positive-number
+(define (line-section-length line station1-name station2-name)
+  (define (find-distance station1 station2 sections)
+    (cond ((null? sections) #f)
+          ((and (equal? (cadr (car sections)) station1) (equal? (cadr (cadr sections)) station2))
+           (caddr (car sections)))
+          (else (find-distance station1 station2 (cdr sections)))))
 
+  (define (accumulate-distance station1 station2 sections)
+    (cond ((null? sections) 0)
+          ((and (equal? (cadr (car sections)) station1) (equal? (cadr (cadr sections)) station2))
+           (caddr (car sections)))
+          (else (+ (caddr (car sections))
+                   (accumulate-distance station1 station2 (cdr sections))))))
+
+  (let ((sections (cdr (cdr (cdr line)))))
+    (let ((distance (accumulate-distance station1-name station2-name sections)))
+      (if distance
+          distance
+          "Trayecto no encontrado"))))
 
 ;;REQUERIMIENTO 7:  Función que permite determinar el costo total (monetario) de recorrer una línea.
 ;Dom: line (line)
 ;Rec: positive-number U {0}
 
-(define line-cost
-  (lambda(line)
-    (define get-cost
-      (lambda(section)
-        (if(null?(cdr section)) (car section)
-           (get-cost (cdr section)))))
+(define primera-estacion
+  (lambda (line)
     (cond
-      ((null? line)null)
-      (else (apply + (map get-cost(cdr(cdr(cdr line)))))))))
+      ((null? (cdr (cdr (cdr line)))) '())
+      (else
+       (car (cdr (car (car (cdr (cdr (cdr line)))))))))))
+
+(define ultima-estacion
+  (lambda (line)
+    (cond
+      ((null? (cdr (cdr (cdr line)))) '())
+      (else
+       (car (cdr (car (cdr (car (reverse line))))))))))
+
+(define es-circular?
+  (lambda (line)
+    (cond
+      ((equal? (primera-estacion line) (ultima-estacion line)) #t)
+      ((and (null? (primera-estacion line)) (null? (ultima-estacion line))) #f)
+      (else #f))))
+
+(define line-cost
+  (lambda (line)
+    (define get-section-cost
+      (lambda (section)
+        (if (null? section)
+            0
+            (cadr (cddr section)))))
+    (define largo-circular
+      (lambda (sections estacion-pivote)
+        (define (calculate-total section total)
+          (if (equal? (cadr section) estacion-pivote)
+              total
+              (+ total (get-section-cost section))))
+        (foldl calculate-total 0 sections)))
+    (if (es-circular? line)
+        (largo-circular (cdr (cdr (cdr line))) (primera-estacion line))
+        (apply + (map get-section-cost (cdr (cdr (cdr line))))))))
+
+;;REQUERIMIENTO 8:   Función que permite determinar el costo de un trayecto entre una estación origen y una final.
+;Dom: line (line) X station1-name (String) X station2-name (String)
+;Rec: positive-number U {0}
+(define (get-section-cost section)
+  (car(reverse section)))
+
+(define (line-section-cost line st1 st2)
+  (define (find-section-cost-tail list st1 st2 acum acc)
+    (cond
+      ((= acum 2) acc)
+      ((null? list)acc)
+      ((member st1 (car list))
+       (find-section-cost-tail (cdr list) st1 st2 (+ 1 acum) (+ (get-section-cost (car list)) acc)))
+      ((member st2 (car list)) (+ 1 acum))
+      (else
+       (find-section-cost-tail (cdr list) st1 st2 acum (+ (get-section-cost (car list)) acc)))))
+  (find-section-cost-tail (cdddr line) st1 st2 0 0))
+
 
 ;;REQUERIMIENTO 9: Función que permite añadir tramos a una línea
 ;Dom: line (line) X section (section)
@@ -95,6 +164,38 @@
 (define l3 (line-add-section l2 s1))
 (define l4 (line-add-section l3 s2))
 (define l5 (line-add-section l4 s3))
+
+(define (same-type-stations? line)
+  (define first-station-type (get-station-type (car(car (get-sections line)))))
+  (define (all-same-type? station-list)
+    (cond
+      ((null? station-list) #t)
+      ((not (equal? (get-station-type (car(car station-list))) first-station-type)) #f)
+      (else (all-same-type? (cdr station-list)))))
+  
+  (all-same-type? (get-sections line)))
+
+(define get-station-type
+  (lambda (station)
+    (caddr station)))
+
+(define get-sections
+  (lambda (line)
+    (cdr (cddr line))))
+
+
+(define es-vacia?
+  (lambda (line)
+    (cond
+      ((null? (cdr (cdr (cdr line)))) #t)
+      (else #f))))
+
+(define line?
+  (lambda (line)
+    (cond
+      ((equal? #t (es-vacia? line)) #f)                 ;; Verifica si la línea es vacía
+      ((equal? #t (same-type-stations? line)) #t)       ;; Verifica si todas las estaciones son del mismo tipo
+      (else #t))))
 
 ;;REQUERIMIENTO 11: Permite crear los carros de pasajeros que conforman un convoy. Los carros pueden ser de tipo terminal (tr) o central (ct).
 ;Dom: id (int) X capacity (positive integer) X model (string) X type (car-type))
@@ -120,6 +221,10 @@
 
 (define (get-pcar train)
   (cddr(cdddr train)))
+
+(define get-type
+  (lambda (pcar)
+    (car(cddr(cdr pcar)))))
 
 (define tr-type
   (lambda (pcars)
@@ -154,6 +259,61 @@
 (define t1 (train 1 "CAF" "UIC 60 ASCE" 70  2 pc1 pc0 pc3 pc2))
 (define t2 (train 1 "CAF" "UIC 60 ASCE" 70  2 pc1 pc0 pc3 pc4))
 
+
+;;REQUERIMIENTO 13: Función que permite añadir carros a un tren en una posición dada.
+;Dom: train (train) X pcar (pcar) X position (positive-integer U {0})
+;Rec: train
+(define train-add-car
+  (lambda (train pcar posicion)
+    (define (train-add-car-tail pcars pcar posicion cont acc)
+      (cond
+        ((null? pcars) (reverse (cons pcar acc))) 
+        ((equal? posicion cont) (train-add-car-tail (cdr pcars) pcar posicion (+ 1 cont) (cons (car pcars) acc)))
+        (else (train-add-car-tail (cdr pcars) pcar posicion (+ 1 cont) (cons (car pcars) acc)))))
+    (train-add-car-tail train pcar posicion 0 '())))
+
+;;REQUERIMIENTO 14: Función que permite eliminar un carro desde el convoy.
+;Dom: train (train) X position (positive-integer U {0})
+;Rec: train
+(define train-remove-car
+  (lambda (train posicion)
+    (define (train-remove-car-tail pcars posicion cont acc)
+      (cond
+        ((null? pcars) '()) 
+        ((equal? posicion cont) (append (reverse acc) (cdr pcars))) 
+        (else (train-remove-car-tail (cdr pcars) posicion (+ 1 cont) (cons (car pcars) acc))))) 
+    (train-remove-car-tail train (+ 5 posicion) 0 '())))
+
+;;REQUERIMIENTO 15: Función que permite determinar si un elemento es un tren válido, esto es, si el elemento tiene la estructura de tren y los carros que lo
+;;conforman son compatibles (mismo modelo) y tienen una disposición coherente con carros terminales (tr) en los extremos y centrales (ct) en medio del convoy.
+;Dom:train
+;Rec:boolean
+(define train?
+  (lambda (train)
+    (cond
+      ((null? (cdr (get-pcar train)))#f)
+      ((null? train)#f)
+      ((and (equal? (tr-type (get-pcar train)) #t)
+            (equal? (ct-type (get-pcar (cdr train))) #t)) #t) ; Verifica si el tipo de tren es válido
+      (else #f)))) ; Devuelve falso en cualquier otro caso
+
+;;REQUERIMIENTO 16:Función que permite determinar la capacidad máxima de pasajeros del tren.
+;Dom: train
+;Rec: positive-number U {0}
+(define get-capacity
+  (lambda (pcar)
+    (car(cdr pcar))))
+
+(define train-capacity
+  (lambda (train)
+    (define capacity-tail
+      (lambda (pcars acc)
+        (cond
+          ((null? pcars) acc)
+          (else
+           (capacity-tail (cdr pcars) (+ (get-capacity (car pcars)) acc))))))
+    (capacity-tail (get-pcar train) 0)))
+
 ;;REQUERIMIENTO 17:
 ;Dom: id (int) X nombre (string) X train-maker (string)
 ;Rec: driver
@@ -179,8 +339,10 @@
 ;Dom:sub (subway) X train+
 ;Rec: subway
 (define subway-add-train
-  (lambda (subway . train)
-    (list subway train)))
+  (lambda (subway . trains)
+    (if (null? trains)
+        (list subway)
+        (cons subway (apply subway-add-train trains)))))
 
 ;;REQUERIMIENTO 20: Función que permite añadir líneas a una red de metro.
 ;Dom: sub (subway) X line+
@@ -188,3 +350,10 @@
 (define subway-add-line
   (lambda (subway . line)
     (list subway line)))
+
+;;REQUERIMIENTO 21: Función que permite añadir conductores a una red de metro.
+;Dom: sub (subway) X driver+ 
+;Rec: subway
+(define subway-add-driver
+  (lambda (subway . driver)
+    (list subway driver)))
